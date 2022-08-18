f1fbf6c7dfbc42b818ce79a789b91a33cb33a57a
jackson <jackson@debian>
2022-08-17 22:16:52 -0300

    Fim da aula - dia 17/08: Estudo sobre views

--- app/src/main/java/com/example/views/MainActivity.java
+++ app/src/main/java/com/example/views/MainActivity.java
@@ -3,18 +3,43 @@ package com.example.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.views.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding amb;
+    private Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         amb = ActivityMainBinding.inflate(getLayoutInflater());
-        
+
         setContentView(amb.getRoot());
+
+        amb.btSalvar.setOnClickListener(view -> {
+            pessoa = new Pessoa(
+                    amb.etNome.getText().toString(),
+                    amb.etSobrenome.getText().toString(),
+                    amb.etEmail.getText().toString(),
+                    amb.spEstadoCivil.getSelectedItem().toString(),
+//                    ((TextView) amb.spEstadoCivil.getSelectedView()).getText(),
+                    amb.rbFem.isChecked() ? getString(R.string.feminino) : getString(R.string.masculino)
+            );
+            ;
+            Toast.makeText(this, pessoa.toString(), Toast.LENGTH_SHORT).show();
+        });
+
+        amb.btLimpar.setOnClickListener(view -> {
+            amb.etNome.setText("");
+            amb.etSobrenome.setText("");
+            amb.etEmail.setText("");
+            amb.spEstadoCivil.setSelection(0);
+            amb.rbFem.setSelected(true);
+            pessoa = null;
+        });
     }
 }
---
+++ app/src/main/java/com/example/views/Pessoa.java
@@ -0,0 +1,57 @@
+package com.example.views;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String estadoCivil;
+    private String sexo;
+
+    public Pessoa(String nome, String sobrenome, String email, String estadoCivil, String sexo) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.estadoCivil = estadoCivil;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
